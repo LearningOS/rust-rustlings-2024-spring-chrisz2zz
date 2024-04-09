0e2653c332e7ec66b87338e3b423453e114c06d4
Chriszzz <chrisz2zz@duck.com>
2024-04-10 00:01:17 +0800

    feat: 打游戏去了 明天再写

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,26 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
+    pub is_num: bool,
 }
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        if self.is_num {
+            format!(
+                "{} ({}) - achieved a grade of {}",
+                &self.student_name, &self.student_age, &self.grade
+            )
+        } else {
+            format!(
+                "{} ({}) - achieved a grade of {}",
+                &self.student_name, &self.student_age, "A+"
+            )
+        }
     }
 }
 
@@ -41,6 +49,7 @@ mod tests {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
+            is_num: true,
         };
         assert_eq!(
             report_card.print(),
@@ -55,6 +64,7 @@ mod tests {
             grade: 2.1,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
+            is_num: false,
         };
         assert_eq!(
             report_card.print(),
